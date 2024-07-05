abd443eb2847c02972b678827bcfe115927d71c1
sagarvionsys <sagaryenkure@vionsys.com>
2024-07-05 14:50:49 +0530

    added foreground msg option

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,10 +1,8 @@
-// Import Firebase scripts required for Firebase app and Firebase messaging
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts(
   "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
 );
 
-// Firebase configuration object
 const firebaseConfig = {
   apiKey: "AIzaSyBo3stE-8aVO0uMbpNBaRxCcatwgWijb8c",
   authDomain: "vionsys-ems-notify.firebaseapp.com",
@@ -14,26 +12,38 @@ const firebaseConfig = {
   appId: "1:444222697746:web:729f37b1d8de2cb83e03cb",
 };
 
-// Initialize Firebase app with the configuration
 firebase.initializeApp(firebaseConfig);
 
-// Retrieve an instance of Firebase Messaging to handle background messages
 const messaging = firebase.messaging();
 
-// Handle background messages
+// Background message handler
 messaging.onBackgroundMessage((payload) => {
   console.log(
     "[firebase-messaging-sw.js] Received background message ",
     payload
   );
 
-  // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
     icon: "/public/assets/vionsys_logo.png",
   };
 
-  // Show notification to the user
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+// Foreground message handler (for messages received while the app is in the foreground)
+messaging.onMessage((payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received foreground message ",
+    payload
+  );
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "/public/assets/vionsys_logo.png",
+  };
+
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
