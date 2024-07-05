5a6230e8cd24779847004461b59ff9a0db6bf91e
sagarvionsys <sagaryenkure@vionsys.com>
2024-07-05 10:54:43 +0530

    firebase msg improved

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,10 @@
+// Import Firebase scripts required for Firebase app and Firebase messaging
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts(
   "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
 );
 
+// Firebase configuration object
 const firebaseConfig = {
   apiKey: "AIzaSyBo3stE-8aVO0uMbpNBaRxCcatwgWijb8c",
   authDomain: "vionsys-ems-notify.firebaseapp.com",
@@ -12,15 +14,26 @@ const firebaseConfig = {
   appId: "1:444222697746:web:729f37b1d8de2cb83e03cb",
 };
 
+// Initialize Firebase app with the configuration
 firebase.initializeApp(firebaseConfig);
+
+// Retrieve an instance of Firebase Messaging to handle background messages
 const messaging = firebase.messaging();
 
+// Handle background messages
 messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = "Vionsys Software Solutions India PVT. LTD";
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: "/assets/icons/vionsys_logo.png",
+    icon: "/public/assets/vionsys_logo.png",
   };
 
+  // Show notification to the user
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
