2197cf95b2ccb706a9b59f64ace5bddc5f2d520b
Ranjitgund <Ranjitgund@vionsys.com>
2024-09-05 18:23:29 +0530

    Log Out Added in user settings in header and search functionality added in header.

--- src/ui/tickets/admin/ManageTickets.jsx
+++ src/ui/tickets/admin/ManageTickets.jsx
@@ -1,49 +1,55 @@
-import React, { useState } from 'react'
-import useGetAssigneeTicketsById from '../../../features/ticket/useGetAssigneeById';
-import getUserIdRole from '../../../utils/getUserIdRole';
-import { Button, Modal, Table, Tag } from 'antd';
+import React, { useState } from "react";
+import useGetAssigneeTicketsById from "../../../features/ticket/useGetAssigneeById";
+import getUserIdRole from "../../../utils/getUserIdRole";
+import { Button, Modal, Table, Tag, Input } from "antd";
 import { CiEdit } from "react-icons/ci";
-import EditTicket from './EditTicket';
+import EditTicket from "./EditTicket";
 
 const ManageTickets = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [record, setRecord] = useState({});
+  const [searchText, setSearchText] = useState("");
   const { id } = getUserIdRole();
   const { data, isPending } = useGetAssigneeTicketsById(id);
+
+  const handleSearch = (e) => {
+    setSearchText(e.target.value.toLowerCase());
+  };
+
   const columns = [
     {
-      title: 'Employee Name',
-      dataIndex: 'name',
-      key: 'name',
+      title: "Employee Name",
+      dataIndex: "name",
+      key: "name",
     },
     {
-      title: 'Email',
-      dataIndex: 'email',
-      key: 'email'
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
     },
     {
-      title: 'Designation',
-      dataIndex: 'designation',
-      key: 'designation'
+      title: "Designation",
+      dataIndex: "designation",
+      key: "designation",
     },
     {
-      title: 'Ticket Type',
-      dataIndex: 'ticketType',
-      key: 'ticketType'
+      title: "Ticket Type",
+      dataIndex: "ticketType",
+      key: "ticketType",
     },
     {
-      title: 'Description',
-      dataIndex: 'description',
-      key: 'description'
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
     },
     {
-      title: 'Priority',
-      dataIndex: 'priority',
-      key: 'priority',
+      title: "Priority",
+      dataIndex: "priority",
+      key: "priority",
       filters: [
-        { text: 'LOW', value: 'LOW' },
-        { text: 'NORMAL', value: 'NORMAL' },
-        { text: 'HIGH', value: 'HIGH' },
+        { text: "LOW", value: "LOW" },
+        { text: "NORMAL", value: "NORMAL" },
+        { text: "HIGH", value: "HIGH" },
       ],
       onFilter: (value, record) => record.priority === value,
       filterSearch: true,
@@ -51,13 +57,13 @@ const ManageTickets = () => {
         let color = "";
         switch (priority) {
           case "LOW":
-            color = '#FF6600';
+            color = "#FF6600";
             break;
           case "NORMAL":
-            color = '#3399FF';
+            color = "#3399FF";
             break;
           case "HIGH":
-            color = '#FF0000';
+            color = "#FF0000";
             break;
           default:
             break;
@@ -66,16 +72,16 @@ const ManageTickets = () => {
           <Tag color={color} key={priority}>
             {priority}
           </Tag>
-        )
-      }
+        );
+      },
     },
     {
-      title: 'Status',
-      dataIndex: 'status',
-      key: 'status',
+      title: "Status",
+      dataIndex: "status",
+      key: "status",
       filters: [
-        { text: "OPEN", value: 'OPEN' },
-        { text: "CLOSED", value: 'CLOSED' },
+        { text: "OPEN", value: "OPEN" },
+        { text: "CLOSED", value: "CLOSED" },
       ],
       onFilter: (value, record) => record.status === value,
       filterSearch: true,
@@ -83,7 +89,7 @@ const ManageTickets = () => {
         let color = "";
         switch (status) {
           case "OPEN":
-            color = '#FAAD14';
+            color = "#FAAD14";
             break;
           case "CLOSED":
             color = "darkred";
@@ -95,66 +101,77 @@ const ManageTickets = () => {
           <Tag color={color} key={status}>
             {status}
           </Tag>
-        )
-      }
+        );
+      },
     },
     {
-      title: 'Edit',
-      dataIndex: 'edit',
+      title: "Edit",
+      dataIndex: "edit",
       render: (_, record) => (
         <Button
-          type='link'
+          type="link"
           icon={<CiEdit size={25} />}
           onClick={() => handleEdit(record)}
         />
-      )
-    }
+      ),
+    },
   ];
 
-
-  const dataSource = data?.map((ticket) => {
-    const ticketId = ticket._id;
-    const name = ticket.ticketRaiserFullName;
-    const email = ticket.ticketRaiser.email;
-    const designation = ticket.ticketRaiser.designation;
-    const ticketType = ticket.ticketType;
-    const description = ticket.description;
-    const priority = ticket.priority;
-    const status = ticket.status;
-    return {
-      ticketId,
-      name,
-      email,
-      designation,
-      ticketType,
-      description,
-      priority,
-      status
-    }
-  });
-
+  const dataSource = data
+    ?.map((ticket) => {
+      const ticketId = ticket._id;
+      const name = ticket.ticketRaiserFullName;
+      const email = ticket.ticketRaiser.email;
+      const designation = ticket.ticketRaiser.designation;
+      const ticketType = ticket.ticketType;
+      const description = ticket.description;
+      const priority = ticket.priority;
+      const status = ticket.status;
+      return {
+        ticketId,
+        name,
+        email,
+        designation,
+        ticketType,
+        description,
+        priority,
+        status,
+      };
+    })
+    .filter((ticket) => ticket.name.toLowerCase().includes(searchText));
 
   const handleEdit = (record) => {
     setIsModalOpen(!isModalOpen);
     setRecord(record);
-  }
+  };
 
   return (
-    <div className='w-full h-full p-4'>
-      <h2 className='text-xl font-semibold mb-2 '>All Tickets</h2>
-      {
-        isPending ? (<div>Loading....</div>) : (<Table columns={columns} dataSource={dataSource} />)
-      }
+    <div className="w-full flex flex-col  h-full p-4">
+      <h2 className="text-xl font-semibold mb-2">All Tickets</h2>
+      <Input
+        placeholder="Search Tickets by Employee Name"
+        onChange={handleSearch}
+        style={{ marginBottom: 16, width: 400 }}
+      />
+      {isPending ? (
+        <div>Loading....</div>
+      ) : (
+        <Table columns={columns} dataSource={dataSource} />
+      )}
       <Modal
         centered
         open={isModalOpen}
         onCancel={() => setIsModalOpen(false)}
         footer={null}
       >
-        <EditTicket record={record} isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
+        <EditTicket
+          record={record}
+          isOpen={isModalOpen}
+          setIsOpen={setIsModalOpen}
+        />
       </Modal>
     </div>
-  )
-}
+  );
+};
 
 export default ManageTickets;
